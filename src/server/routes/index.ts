1d37f60d6104dc80fb338de7456456e6e889ce41
JoshRuthel <josh@kerosports.com>
2024-10-10 11:08:10 +0200

    add health check

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import account from "./account/index"
 import job from "./job"
+import { Request, Response } from 'express'
 
 const router = express.Router();
 router.use("/account", account);
 router.use("/job", job);
+router.get("/healthz", async (req: Request, res: Response) => {
+    res.status(200)
+})
 
 export default router;
