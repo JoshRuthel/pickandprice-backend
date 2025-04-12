9437206cfe4e15f7ac541ee4072d6425898158ad
JoshRuthel <josh@kerosports.com>
2025-04-12 14:02:48 +0200

    remove refresh logic

--- src/server/routes/utils.ts
+++ src/server/routes/utils.ts
@@ -1,6 +1,4 @@
 import jwt from "jsonwebtoken";
-import { v4 as uuid } from "uuid";
-import { db } from "../../database";
 
 const JWT_SECRET = process.env.JWT_SECRET || "secret";
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || "refreshSecret";
@@ -10,14 +8,5 @@ export function generateTokens(userId: string) {
     expiresIn: 7200,
   });
   const refreshToken = jwt.sign({ userId }, REFRESH_TOKEN_SECRET, { expiresIn: "2h" });
-  const expirationTime = new Date();
-  expirationTime.setHours(expirationTime.getHours() + 2);
-  const id = uuid()
-  const insertQuery = "INSERT into refresh_tokens (id, user_id, token, expires_at) values ($1, $2, $3, $4)";
-  try {
-    db.query(insertQuery, [id, userId, refreshToken, expirationTime.toDateString()]);
-  } catch (e) {
-    console.error(e);
-  }
   return { accessToken, refreshToken };
 }
